5cb5c441ff0bf81fd865dca9b4180ecfefe9f474
Nemuidere <olokoxthelegend@gmail.com>
2025-12-05 23:16:24 +0100

    feat: solution for task 8

---
+++ CodeWars/task8.py
@@ -0,0 +1,20 @@
+"""
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/python
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+
+def accum(st):
+    counter = 0
+    parts = []
+    for x in st:
+        parts.append(f"{x.upper()}{x.lower()*counter}")
+        counter += 1
+    result = "-".join(parts)
+    return result
