6a73e43b6d054a26bb96b468d3a62fcb4ea27654
Nemuidere <olokoxthelegend@gmail.com>
2026-05-07 12:31:45 +0200

    feat: partial solution for 2kyu hard_sudoku_solver

--- CodeWars/2kyu/hard_sudoku_solver.py
+++ CodeWars/2kyu/hard_sudoku_solver.py
@@ -18,10 +18,44 @@ Write a function that solves sudoku puzzles of any difficulty. The function will
 Or it should raise an error in cases of: invalid grid (not 9x9, cell with values not in the range 1~9); multiple solutions for the same puzzle or the puzzle is unsolvable
 """
 
-int 123 ehehe TO_BE_FINISHED;
+from itertools import product as prd
 
-def sudoku_solver(puzzle):
-    solved = False
-    while (!solved):
-        for (i=1, i<=9, ++i):
+def sudoku_solver(horizontal):
+    stack = []
+    all_nums = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    solved_count = 0
+    while True:
+        best_cell = None
+        min_candidates = 10
+        for x, y in prd(range(9), range(9)):
+            if horizontal[x][y] == 0:
+
+                set_row = set(horizontal[x])
+                set_col = set(horizontal[r][y] for r in range(9))
+                r_start, c_start = (x // 3) * 3, (y // 3) * 3
+                set_sqr = {horizontal[r][c] for r, c in prd(range(r_start, r_start + 3), range(c_start, c_start + 3))}
+
+                current_nums = all_nums - set_row - set_col - set_sqr
+                num_candidates = len(current_nums)
+                
+                if num_candidates == 0:
+                    best_cell = (x, y, [])
+                    break 
+                
+                if num_candidates < min_candidates:
+                    min_candidates = num_candidates
+                    best_cell = (x, y, list(current_nums))
+                    
+                if num_candidates == 1:
+                    break
+
+        if best_cell is None:
+            solved_count += 1
+            solution = [row[:] for row in horizontal]
+            continue
         
+        # 0 candidates handling
+        
+        # x>1 candidates handling
+        
+    return solution
